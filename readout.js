a7b436c608e7b212eec83a4b9d43e9ffc6b2dbcf
Trevor <twhitt14@gmail.com>
2017-11-09 17:02:37 -0700

    Update readout.js

--- readout.js
+++ readout.js
@@ -1,5 +1,1 @@
 console.log('GitHub code successfully loaded');
-document.cookie = "AAAA=test";
-document.cookie = "AAAB=test;domain=githubusercontent.com";
-document.cookie = "AAAC=test;domain=github.com";
-document.cookie = "AAAC=test;domain=rawgit.com";
